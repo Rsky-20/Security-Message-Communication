25ca8f527573bdc5d5dd5cb01d141828ad77de34
Rsky-20 <61126092+Rsky-20@users.noreply.github.com>
2020-10-10 00:04:36 +0200

    Update 1.4
    Modification de syntaxe

--- Server/server_V2.py
+++ Server/server_V2.py
@@ -15,40 +15,39 @@ def documentation():
 
 
 def log(response):
-    with open("log.txt", "a") as log:
-        log.write("\n" + "\t[" + datetime.datetime.isoformat(datetime.datetime.now())
-                  + "]\n" + "{\n" + response + "\n}\n" + "#________________________________________________#\n")
-        log.close()
+    with open("log.txt", "a") as lmsg:
+        lmsg.write("\n" + "\t[" + datetime.datetime.isoformat(datetime.datetime.now())
+                   + "]\n" + "{\n" + response + "\n}\n" + '#________________________________________________#\n')
+        lmsg.close()
 
 
 def log_connexion(clients_connected):
-    with open("log_connexion.txt", "a") as log:
-        log.write(datetime.datetime.isoformat(datetime.datetime.now()) +
-                  "Le client {} est connecté sur le serveur via le port {}".format(clients_connected, PORT))
-        log.close()
+    with open("log_connexion.txt", "a") as lc:
+        lc.write(datetime.datetime.isoformat(datetime.datetime.now()) +
+                 "Le client {} est connecté sur le serveur via le port {}".format(clients_connected, PORT))
+        lc.close()
 
 
-def process_server(client, msg_recv):
+def process_server(msg_recv):
     response = str(msg_recv)
     log(response)
 
     return response
 
 
-# def bridge_com(client):
-
-
 def connexion_server(HOST, PORT):
-    documentation()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(5)
     print('{serverver-status}:', '\033[32m', 'Online', '\033[1m')
+    print(s)
 
     server_ready = True
     client_connected = []
+    read_client = []
+
     while server_ready:
 
         wait_connexions, wlist, xlist = select.select([s], [], [], 0.05)
@@ -56,13 +55,12 @@ def connexion_server(HOST, PORT):
         for connexion in wait_connexions:
             connexion_client, info_connexion = connexion.accept()
             client_connected.append(connexion_client)
+            print(client_connected)
 
-
-########################################################################################################################
+            ########################################################################################################################
 
             for client in client_connected:
                 msg_recv = client.recv(1024)
-                print(client_connected)
 
                 msg_recv = msg_recv.decode()
 
@@ -72,15 +70,12 @@ def connexion_server(HOST, PORT):
                 if msg_recv == "/disconnexion":
                     server_ready = False
 
-                p_server = process_server(client, msg_recv)
+                p_server = process_server(msg_recv)
 
                 byte_data = p_server.encode()
 
                 client.sendall(byte_data)
 
-
-
-        read_client = []
         try:
             read_client, wlist, xlist = select.select(client_connected, [], [], 0.05)
 
@@ -89,7 +84,6 @@ def connexion_server(HOST, PORT):
         else:
 
             for client in read_client:
-                print(read_client)
 
                 msg_recv = client.recv(1024)
 
@@ -101,15 +95,13 @@ def connexion_server(HOST, PORT):
                 if msg_recv == "/disconnexion":
                     server_ready = False
 
-                p_server = process_server(client, msg_recv)
+                p_server = process_server(msg_recv)
 
                 byte_data = p_server.encode()
 
                 client.sendall(byte_data)
 
-########################################################################################################################
-
-    print(client_connected)
+    ########################################################################################################################
 
     print("Close all connections")
     for client in client_connected:
@@ -122,7 +114,8 @@ def run():
     print('[' + '\033[31m' + 'SERVER@' + '\033[36m' + HOST + ' ' + '\033[33m' + '-p' + str(PORT) + '\033[39m' + ']:\n')
 
     while True:
+
         connexion_server(HOST, PORT)
 
-
+documentation()
 run()
